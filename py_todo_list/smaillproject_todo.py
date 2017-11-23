f338d2452f4de757c25a7d53a7ef221d0d25492a
Plusone7 <y02121148s@gmail.com>
2017-11-23 19:47:44 +0800

    add search map func

--- py_todo_list/smaillproject_todo.py
+++ py_todo_list/smaillproject_todo.py
@@ -1,19 +1,29 @@
 import sys,pyperclip
-# import class
 from todo_model import dataProcess as todo
 def funSwitch():
-    if funcChoose == 'del': todo.del_data()
-    elif funcChoose == 'add': todo.add_data()
-    elif funcChoose == 'done': todo.work_done()
-    elif funcChoose == 'show': todo.show_list()
+    if funcChoose == 'del':
+        todo.del_data()
+        todo.show_list()
+    elif funcChoose == 'add':
+        todo.add_data()
+        todo.show_list()
+    elif funcChoose == 'done':
+        todo.work_done()
+        todo.show_list()
+    elif funcChoose == 'show':
+        todo.show_list()
+    elif funcChoose == 'map':
+        todo.google_map()
+    elif funcChoose == 'search':
+        todo.google_search()
 if len(sys.argv)>1:
     funcChoose = ''.join(sys.argv[1:2])
     jobName = ' '.join(sys.argv[2:])
-    funSwitch()      
+    funSwitch()     
 else:
-    print('what do you want to do ? del,add or done ?')
+    print('what do you want to do ? \'del\',\'add\' or \'done\' job?')
     funcChoose = input()
     print(funcChoose , 'which job ?' )
+    todo.show_list()
     jobName = input()
     funSwitch()
-   
--- py_todo_list/todo_model.py
+++ py_todo_list/todo_model.py
@@ -1,44 +1,34 @@
 
 import pymongo
 from pymongo import MongoClient
-import smaillproject_todo as todoapi
+import smaillproject_todo 
 from datetime import datetime
-
+import webbrowser,requests,bs4
 myClinet = MongoClient()
 db = myClinet["todo_list"] # db name
 job_list = db["todo_data"]  # collections
+
 class dataProcess(object):
-    def __init__(self,job,time,ifdone):
+    def __init__(self,job,time):
         self.job = job
         self.time = time
-        self.ifdone = ifdone
     def add_data():
-        job = todoapi.jobName
+        job = smaillproject_todo.jobName
         time = str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-        ifdone = 'undone'
-        # data that want to insert
         wantto_add = {
             "wantDo":job,
             "add_time":time,
-            "if_finish":ifdone 
-        }
-        # insert data to mongo 
-        has_been_added = job_list.insert_one(wantto_add).inserted_id
-        print('insert job:',job,'insert time: ',time,'state:'+ifdone)
+            "if_finish":'undone' }
+        job_list.insert_one(wantto_add).inserted_id
     def del_data():
-        job = str(todoapi.jobName)
-        if job == 'all' or 'ALL':
-            want_del = job_list.remove({})
+        job = str(smaillproject_todo.jobName)
+        want_del = job_list.remove({"wantDo":job})
+        if(want_del['n']>0):
+            print('Delete', job , 'successfully')
         else:
-            want_del = job_list.remove({"wantDo":job})
-            if(want_del['n']>0):
-                print('Delete', job , 'successfully')
-            else:
-                print('Not found',job)
+            print('Not found',job)
     def work_done():
-        job = str(todoapi.jobName)
-        ifdone = 'done'
-        # print(job,'have been',ifdone)
+        job = str(smaillproject_todo.jobName)
         job_done = job_list.update(
             {"wantDo":job},
             {'$set':{"if_finish":"done"}}
@@ -48,22 +38,24 @@ class dataProcess(object):
         else:
             print('Not found',job)
     def show_list():
-        print("\033[90m┌────────────┬─────────────────┬─────────────┬──────────┬────────┐") 
-        print("│\033[96m id \033[0m        \033[90m│ \033[96mjob\033[0m             \033[90m│ \033[96mdate\033[0m        \033[90m│ \033[96mtime\033[0m     \033[90m│ \033[96mfinish\033[0m \033[90m│")
-        print("\033[90m├────────────┼─────────────────┼─────────────┼──────────┼────────┤") 
+        print("\033[90m┌────────────┬──────────────────┬─────────────┬──────────┬────────┐") 
+        print("│\033[96m id \033[0m        \033[90m│ \033[96mjob\033[0m              \033[90m│ \033[96mdate\033[0m        \033[90m│ \033[96mtime\033[0m     \033[90m│ \033[96mfinish\033[0m \033[90m│")
+        print("\033[90m├────────────┼──────────────────┼─────────────┼──────────┼────────┤") 
         count = 1
         for todo in job_list.find(): 
-            print("│"+str(todo['_id'])[0:10]+"  │ \033[93m"+'{:15}'.format(todo['wantDo'][0:12]) +"\033[90m │ "+todo['add_time'][0:11]+" │ "+todo['add_time'][11:20]+" │ \033[91m"+'{:7}'.format(todo['if_finish'])+"\033[90m│")
-            if count == job_list.find().count():
-                print("└────────────┴─────────────────┴─────────────┴──────────┴────────┘\033[0m")
-            else:
-                print("├────────────┼─────────────────┼─────────────┼──────────┼────────┤") 
+            print("│"+str(todo['_id'])[0:10]+"  │ \033[93m"+'{:16}'.format(todo['wantDo'][0:15]) +"\033[90m │ "+todo['add_time'][0:11]+" │ "+todo['add_time'][11:20]+" │ \033[91m"+'{:7}'.format(todo['if_finish'])+"\033[90m│")
+            if count == job_list.find().count():print("└────────────┴──────────────────┴─────────────┴──────────┴────────┘\033[0m")
+            else:print("├────────────┼──────────────────┼─────────────┼──────────┼────────┤") 
             count = count +1 
-        # HEADER = '\033[95m'
-        # OKBLUE = '\033[94m'
-        # OKGREEN = '\033[92m'
-        # WARNING = '\033[93m'
-        # FAIL = '\033[91m'
-        # ENDC = '\033[0m'
-        # BOLD = '\033[1m'
-        # UNDERLINE = '\033[4m'
+    def google_map():
+        address = smaillproject_todo.jobName
+        webbrowser.open('https://www.google.com/maps/place/'+address)
+    
+    def google_search():
+        search_item = smaillproject_todo.jobName
+        res = requests.get('https://www.google.com/search?q='+' '.join(search_item))
+        soup = bs4.BeautifulSoup(res.text,"html5lib")
+        linkElems = soup.select('.r a') # <h3 class="r">  <a> elments
+        openNum = min(5,len(linkElems)) #open number 5
+        for i in range(openNum):
+            webbrowser.open('http://google.com'+linkElems[i].get('href'))
