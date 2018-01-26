2916e4d11553c7cc340ea81461a96480181d5a9b
plusone7 <y02121148s@gmail.com>
2018-01-26 17:22:02 +0800

    flask-html-extends

---
+++ web-flask/blog-framework/data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles=[
+        {
+            'id':1,
+            'title':'article one',
+            'body':'skdflakdlfkalskdflaks;dlfka;lsdkfl;aksdf;a',
+            'author':'plusone',
+            'date':'01-27-2018'
+        },
+          {
+            'id':2,
+            'title':'article two',
+            'body':'skdflakdlfkalskdflaks;dlfka;lsdkfl;aksdf;a',
+            'author':'plusone',
+            'date':'01-27-2018'
+        },
+          {
+            'id':3,
+            'title':'article three',
+            'body':'skdflakdlfkalskdflaks;dlfka;lsdkfl;aksdf;a',
+            'author':'plusone',
+            'date':'01-27-2018'
+        }
+    ]
+    return articles
