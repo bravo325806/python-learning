b2d61619e503b8a3740ddb5b8ae64509767e9ce1
plusone7 <y02121148s@gmail.com>
2018-02-05 16:09:19 +0800

    port compare

--- jupyter_config_create/jupyter_notebook_config_user1.py
+++
@@ -1,6 +0,0 @@
-
-# example file
-c.NotebookApp.ip ='*'
-c.NotebookApp.open_browser = False
-c.NotebookApp.password =''
-c.NotebookApp.port =8000
--- jupyter_config_create/main.py
+++ jupyter_config_create/main.py
@@ -1,36 +1,70 @@
 import json
 from notebook.auth import passwd
 import os
+
 jupyter = "jupyter_notebook_config"
-def congfig_setting(pswHash,f,fileName):
-    data = json.load(open('./d.json'))
-    userPort = int(data['users'][len(data['users'])-1]['port'])+1
+nowUserPort = "8000" 
+
+# loading json file
+def load_json_file():
+    json_d = open('d.json')
+    global data 
+    data = json.load(json_d)
+    print(data)
+    return data
+
+def find_user_port(nowUserPort):
+    usedPortList = []
+    for _id in data['users']:
+        usedPortList.append(_id['port'])
+        # usedPortList.sort()
+    for used in usedPortList:
+        if nowUserPort in usedPortList:
+            nowUserPort = int(nowUserPort) +1 
+            print("now:",nowUserPort)
+            nowUserPort = str(nowUserPort)
+        else:
+            nowUserPort = str(nowUserPort)
+    print("now user's port:", nowUserPort)
+    return nowUserPort
+
+def congfig_setting(hashpwd,fileName,nowPort,data,f):
     _id = fileName
     newData = {
         "id":_id,
-        "port":userPort
+        "port":nowPort
     }
+    print("new data:",newData)
     data["users"].append(newData)
-    print("insert:",data)
     ip = "c.NotebookApp.ip ='*'" 
     browser = "c.NotebookApp.open_browser = False"
-    port = "c.NotebookApp.port =" + str(userPort)
-    password = "c.NotebookApp.password =" +"\'"+ str(pswHash)+"\'"
+    port = "c.NotebookApp.port =" + str(nowPort)
+    password = "c.NotebookApp.password =" +"\'"+ str(hashpwd)+"\'"
     f.write(ip +"\n"+ browser + "\n" + password +"\n" + port)
+    
     print(ip +"\n"+ browser + "\n" + password + "\n"+ port)
     d = open('./d.json', "w+")                
     data = str(data).replace("'", "\"")
     d.write(data)
     print("save!")
-    
+
+def run_jupyter_user_server(fileName,userId):
+    cmd = 'jupyter notebook --config '+ jupyter + "_" + fileName + ".py --allow-root"
+    os.system(cmd)
+
 print('please enter your user ID:')
 userId = input()
 fileName = userId
 print('please enter your user Password:')
 psw = input()
 hashpwd = passwd(psw)
+
 if os.path.exists(jupyter+"_"+fileName+".py"):
         print(userId + "EXISTS")
 else:
-    with open(jupyter+"_"+fileName+".py","wt") as f:                 
-        congfig_setting(hashpwd,f,fileName)
+    with open(jupyter+"_"+fileName+".py","wt") as f: 
+        load_json_file()
+        nowPort =  find_user_port(nowUserPort)                
+        congfig_setting(hashpwd,fileName,nowPort,data,f)
+
+# run_jupyter_user_server(fileName,userId)
