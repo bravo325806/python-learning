302692eecd94f006bb780f6c06aa27e39cbb5880
Pluseon7 <y02121148s@gmail.com>
2018-04-07 00:52:17 +0800

    class adjust

--- python-basic/01classes.py
+++ python-basic/01classes.py
@@ -1,5 +1,3 @@
-
-
 class Employee:
     staff_num = 0
     raise_amount = 1.8
@@ -11,21 +9,36 @@ class Employee:
         Employee.staff_num += 1
 
     def info(self):
-        print(self.first_name,self.last_name)
+        print('{} {}'.format(self.first_name,self.last_name))
         print('email: {}'.format(self.email))
     def pay_raise(self):
         self.payment = int(self.payment * Employee.raise_amount) #or self.raise_amount
-
-    @staticmethod
-    def is_work_day(day):
-        if day.weekday() == 5 or day.weekday() == 6:
-            return False
-        return True
 
 # create Employee
 staff1 = Employee('brown','jack',22000)
 staff2 = Employee('hello','kitty',22000)
 
-import datetime
-day = datetime.date(2018,4,8)
-print(Employee.is_work_day(day))
+# same
+Employee.info(staff1)
+staff1.info()
+staff2.info()
+
+# origin payment 
+print(staff1.payment)
+# raise
+staff1.pay_raise()
+print(staff1.payment)
+# same
+print(Employee.raise_amount)
+print(staff1.raise_amount)
+
+# we can change the raise_amount
+staff1.raise_amount = 1.5
+print('staff1:',staff1.raise_amount) # 1.5
+print('staff2:',staff2.raise_amount) # 1.8
+
+# __dict__
+# print(staff1.__dict__)
+# print(Employee.__dict__)
+
+print(Employee.staff_num) # 2 :staff1 and staff2
