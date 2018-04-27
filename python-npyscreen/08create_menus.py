4c351f8660fb882b3ddf6ebf757d749c253bc0f5
Pluseon7 <y02121148s@gmail.com>
2018-04-27 19:08:22 +0800

    ok

--- python-npyscreen/08create_menus.py
+++ python-npyscreen/08create_menus.py
@@ -1,10 +1,25 @@
 
 
 import npyscreen
-class FormObj(npyscreen.ActionForm):
+class FormObj(npyscreen.ActionForm, npyscreen.SplitForm, npyscreen.FormWithMenus):
 	def create(self):
 		self.fname = self.add( npyscreen.TitleText, name= 'Name:', value= 'plusone')
 		self.add(npyscreen.TitleText, name='bithday', value="18/2/22")
+		# menus 	
+		self.menu = self.new_menu(name = 'Main Menu', shortcut ='m')
+		self.menu.addItem(text='Item 1', onSelect=self.press_1, shortcut='1')
+		self.menu.addItem(text='Item 2',  onSelect=self.press_2, shortcut='2')
+		self.menu.addItem(text='Exit Form',  onSelect= self.exit_form, shortcut='^x')
+		# submenu
+		self.submenu = self.menu.addNewSubmenu('A new Menu')	
+		self.submenu.addItem("This is submenu")
+	def press_1(self):
+		npyscreen.notify_confirm("You pressed Item 1")
+	def press_2(self):
+                npyscreen.notify_confirm("You pressed Item 2")
+	def exit_form(self):
+		self.parentApp.switchForm(None)
+	
 	def on_ok(self):
 		# ok btn press
 		npyscreen.notify_confirm('Form has been saved!', editw=1)
