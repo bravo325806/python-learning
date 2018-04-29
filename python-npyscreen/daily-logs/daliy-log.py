8b079bdc9ad68b652d48ee210c71810da926f8d6
Pluseon7 <y02121148s@gmail.com>
2018-04-29 20:39:50 +0800

    2 pages

--- python-npyscreen/daily-logs/daliy-log.py
+++ python-npyscreen/daily-logs/daliy-log.py
@@ -4,6 +4,7 @@ import npyscreen
 from datetime import datetime
 import json, os
         
+
 class ExitButton(npyscreen.ButtonPress):
     def whenPressed(self):
         self.parentApp.setNextForm(None)
@@ -11,7 +12,8 @@ class ExitButton(npyscreen.ButtonPress):
 class InputBox(npyscreen.BoxTitle):
     _contained_widget = npyscreen.MultiLineEdit
 
-class FormObj(npyscreen.ActionForm, npyscreen.FormWithMenus, npyscreen.SplitForm):
+# no.1 page - write logs page
+class WriteLogFormObj(npyscreen.ActionForm, npyscreen.FormWithMenus, npyscreen.SplitForm):
     def create(self):   
         y, x = self.useable_space()
         # Name
@@ -20,25 +22,18 @@ class FormObj(npyscreen.ActionForm, npyscreen.FormWithMenus, npyscreen.SplitForm
         self.nextrely +=2
         self.fname = self.add( npyscreen.TitleText, name= 'Your Name:', value= 'plusone', editable = False)
         # log time
-        self.logTime = self.add(npyscreen.TitleText, name='Log Time:' )
-        self.logTime.value = datetime.now().strftime("%Y-%m-%d")
+        self.logTime = self.add(npyscreen.TitleText, name='Log Time:' ,value= datetime.now().strftime("%Y-%m-%d"))
+        # self.logTime.value = datetime.now().strftime("%Y-%m-%d")
         self.nextrely +=1
         self.todayDone = self.add(InputBox, name="What are you doing today?", max_height=y //3 )
         self.problem = self.add(InputBox, name='Any Problem?')
-        # menus 	
-        def weekly_logs():
-            npyscreen.notify_confirm('This function not completed yet')
+
         def  all_logs():
-            npyscreen.notify_confirm('This function not completed yet')
-        
+            self.parentApp.switchForm('SECOUND')
         self.menu = self.new_menu(name ='Menu' , shortcut ='^x')
-        self.menu.addItem(text='Weekly Logs', onSelect= weekly_logs, shortcut='1')
-        self.menu.addItem(text='All Logs', onSelect= all_logs, shortcut='2')
-        self.menu.addItem(text='Exit Menu', shortcut='3')
-        
-    def exit_form(self):
-        self.parentApp.switchForm(None)
-	
+        self.menu.addItem(text='All Logs', onSelect= all_logs, shortcut='1')
+        self.menu.addItem(text='Exit Menu', shortcut='2')
+    
     def on_ok(self):
         # ok btn press
         data = {
@@ -55,7 +50,7 @@ class FormObj(npyscreen.ActionForm, npyscreen.FormWithMenus, npyscreen.SplitForm
             with open( './logs/'+ self.logTime.value+'.json', 'w') as f: 
                 json.dump(data, f)
         npyscreen.notify_confirm('Good!'+ self.fname.value+ '\n\nYour log has been saved!\nNow click "Cancel" to leave! ')
-        self.saved.value = 'Saved!'
+        self.saved.value = 'Saved! at ' + datetime.now().strftime("%Y-%m-%d.%H:%M")
        
     def on_cancel(self):
         # cancel btn press
@@ -68,11 +63,26 @@ class FormObj(npyscreen.ActionForm, npyscreen.FormWithMenus, npyscreen.SplitForm
                 self.parentApp.setNextForm(None)
             else:
                 npyscreen.notify_confirm("You may continue working", 'Allright!')
-        
+
+# no.2 page - show logs page
+class ShowLogsForm(npyscreen.ActionForm, npyscreen.FormWithMenus):
+    def create(self):   
+        y, x = self.useable_space()
+        self.add( npyscreen.TitleText, name= 'NOW:', value= datetime.now().strftime("%Y-%m-%d %H:%M"), editable = False)    
+
+    def on_ok(self):
+       pass
+       
+    def on_cancel(self):
+        # cancel btn press
+        self.parentApp.setNextForm('MAIN')
+
+
+
 class App(npyscreen.NPSAppManaged):
     def onStart(self):
-        self.addForm('MAIN', FormObj, draw_line_at = 4, name ='DALIY LOGS') 
-        
+        self.addForm('MAIN', WriteLogFormObj, draw_line_at = 4, name ='write_log') 
+        self.addForm('SECOUND', ShowLogsForm, name ='show_logs') 
 
 if __name__ == '__main__':
 	app = App()
